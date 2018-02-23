55d57a8c68ddaab752052d55347633db49dbfa80
Alexander Radu <34072014+aradu01@users.noreply.github.com>
2018-02-23 14:47:04 -0500

    Making Progress on countSolutions.

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -2,6 +2,7 @@ public class KnightBoard {
 
     private int[][] board;
     private int[][] coordinates;
+    private int solutionsCounter;
 
     public KnightBoard(int startingRows, int startingCols) {
     board = new int[startingRows][startingCols];
@@ -46,6 +47,8 @@ public class KnightBoard {
     }
 
     public boolean solve(int startingRow, int startingCol) {
+        solutionsCounter = 0;
+        
         for (int[] row: board) {
             for (int cell: row) {
                 if (cell != 0) {
@@ -80,8 +83,10 @@ public class KnightBoard {
         board[row][col] = 0;
         return false;
     }
-   /* 
+
     public int countSolutions(int startingRow, int startingCol) {
+        solutionsCounter = 0;
+        
         for (int[] row: board) {
             for (int cell: row) {
                 if (cell != 0) {
@@ -90,37 +95,45 @@ public class KnightBoard {
             }
         }
         
-        helpCount(starting);
+        helpCount(startingRow, startingCol, 1);
         return solutionsCounter;
     }
     
-    private boolean helpCount(int row, int col, int counter, int xdirection, int ydirection) {
-        if (counter == 0) {
+    private boolean helpCount(int row, int col, int counter) {
+        if (counter == board.length * board[0].length) {
+            board[row][col] = counter;
+            solutionsCounter++;
             return true;
         }
+        
+        board[row][col] = counter;
 
         for (int[] move: coordinates) {
             if (row + move[0] >= 0 && row + move[0] < board.length &&
                 col + move[1] >= 0 && col + move[1] < board[0].length) {
-                if ((xdirection != -move[0] || ydirection != -move[1]) && board[row + move[0]][col + move[1]] == 0) {
-                    if (helpSolve(row + move[0], col + move[1], counter - 1, move[0], move[1])) {
-                        board[row][col] = counter;
-                        System.out.println(this);
-                        return true;
+                if (board[row + move[0]][col + move[1]] == 0) {
+                    if (helpCount(row + move[0], col + move[1], counter + 1)) {
+                        helpCount(row + 1, 0, 1);
+                        board[row][col] = 0;
                     }
                 }
             }
         }
-
+        
+        board[row][col] = 0;
         return false;
     }
-*/
+    
+    /* Testing.
     public static void main(String[] args) {
-        KnightBoard a = new KnightBoard(5, 5);
+        KnightBoard a = new KnightBoard(4,6);
+        a.solve(0,0);
         System.out.println(a);
-
-        a.solve(1, 1);
-        System.out.println(a);
+        
+        KnightBoard c = new KnightBoard(5,5);
+        System.out.println(c.countSolutions(1,1));
+        System.out.println(c);
     }
+    */
 
 }
