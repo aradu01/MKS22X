225d3153e772895b1da0591118e7a1ad0b40cc8c
aradu01 <aradu@stuy.edu>
2018-03-01 15:10:52 -0500

    Perfected Constructor.

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -1,28 +1,10 @@
 import java.util.*;
 import java.io.*;
 
-public class Maze{
+public class Maze {
 
     private char[][] maze;
     private boolean animate;
-
-    /*Constructor loads a maze text file, and sets animate to false by default.
-
-      1. The file contains a rectangular ascii maze, made with the following 4 characters:
-      '#' - Walls - locations that cannot be moved onto
-      ' ' - Empty Space - locations that can be moved onto
-      'E' - the location of the goal (exactly 1 per file)
-
-      'S' - the location of the start(exactly 1 per file)
-
-      2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
-
-
-      3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then: 
-
-         throw a FileNotFoundException or IllegalStateException
-
-    */
 
     public Maze(String filename) throws FileNotFoundException {
         File text = new File(filename);
@@ -32,13 +14,14 @@ public class Maze{
         String line = counter.nextLine();
         int length = line.length();
         int width = 1;
-        
+
         while (counter.hasNextLine()) {
             width++;
+	    counter.nextLine();
         }
         
         char[][] maze = new char[length][width];
-
+	
         while (reader.hasNextLine()) {
             for (int row = 0; row < maze.length; row++) {
                 line = reader.nextLine();
@@ -49,40 +32,23 @@ public class Maze{
             }
         }
     }
-    
-    private void wait(int millis){
+
+    private void wait(int millis) {
          try {
              Thread.sleep(millis);
          }
-         catch (InterruptedException e) {
-         }
+	 
+         catch (InterruptedException e) { }
      }
 
-
-    public void setAnimate(boolean b){
-
+    public void setAnimate(boolean b) {
         animate = b;
-
     }
 
-
-    public void clearTerminal(){
-
-        //erase terminal, go to top left of screen.
-
+    public void clearTerminal() {
         System.out.println("\033[2J\033[1;1H");
-
     }
 
-
-
-    /*Wrapper Solve Function returns the helper function
-
-      Note the helper function has the same name, but different parameters.
-      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
-
-    */
-    
     public int solve(){
 
 	int rowS = 0;
@@ -119,7 +85,7 @@ public class Maze{
 
             Note: This is not required based on the algorithm, it is just nice visually to see.
         All visited spots that are part of the solution are changed to '@'
-    */
+    
     private int solve(int row, int col, int steps) { //you can add more parameters since this is private
 
 	int current = 0;
@@ -152,6 +118,18 @@ public class Maze{
 
         return -1; //so it compiles
     }
+    */
+    
+    public static void main (String[] args) {
+	Maze a = null;
 
-
+	try {
+	    a = new Maze("data1.dat");
+	}
+	catch (FileNotFoundException e) {
+	    System.out.println("Exception works!");
+	    System.exit(1);
+	}
+    }
+    
 }
