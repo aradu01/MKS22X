070b2a50dfdecd257759c133817605ea51259f40
aradu01 <aradu@stuy.edu>
2018-03-07 15:35:47 -0500

    Day 3

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -2,13 +2,20 @@ public class USACO {
 
     public static int bronze(String filename) {
 	File text = new File(filename);
-	File text2 = new File(filename);
-	Scanner counter = new Scanner(text);
-	Scanner reader = new Scanner(text2);
+	Scanner reader = new Scanner(text);
 
-	int length = counter.nextLine().nextLine;
-	int width = 1;
+	int length = reader.nextInt();
+	int width = reader.nextInt();
+	int elevation = reader.nextInt();
+	int numDirections = reader.nextInt();
 
-	while (
+	int[][] field = new int[length][width];
 
-	int[][] field = new int[
+	for (int row = 0; row < field.length; row++) {
+	    for (int col = 0; col < field[row].length; col++) {
+		field[row][col] = reader.nextInt();
+	    }
+	}
+    }
+
+}
