98a84e737d19144b38d85f351c0b062a3fe129d2
aradu01 <aradu@stuy.edu>
2018-03-23 15:35:14 -0400

    Fixing merge.

--- 07MergeSort/Merge.java
+++ 07MergeSort/Merge.java
@@ -1,12 +1,4 @@
 public class Merge {
-
-    private static void swap(int[] data, int first, int second) {
-        int storage;
-        
-        storage = data[first];
-        data[first] = data[second];
-        data[second] = storage;
-    }
     
     private static void print(int[] data) {
         String result = "";
@@ -19,19 +11,15 @@ public class Merge {
     }
     
     private static void msort(int[] data, int[] temp, int start, int end) {
-        if (start < 0 || end >= data.length || start > end) {
-            throw new IllegalStateException();
-        }
-        
         int middle = (start + end) / 2;
 	
-	while (end - start > 1) {
+	if (end > start) {
 	    msort(temp, data, start, middle);
 	    msort(temp, data, middle + 1, end);
+	    
+	    merge(data, temp, start, middle, middle + 1, end);
 	}
-
-	merge(data, temp, start, middle, end);
-	
+		
 	/* Previous Code.
         for (int index = 0; index < data.length - 1; index += 2) {
             if (data[index] < data[index + 1]) {
@@ -47,21 +35,49 @@ public class Merge {
 	*/
     }
 
-    private static void merge(int[] data, int[] temp, int low, int middle, int high) {
-        if (low != high) {
-	    print(data);
-	    print(temp);
-	    System.out.println("Low: " + low + " Middle: " + middle + " High: " + high);
-	    
-	    /* Turning into Selection Sort.
-	    int lowest = 0;
-	    int start = 0;
-
-	    for (int element = 0; index < data.length; element++) {
-		for (int index = start; index < data.length; index++) {
-		    if 
-	    */
+    private static void merge(int[] data, int[] temp, int low, int middle, int quartile, int high) {
+	int counter = low;
+	
+	if (low > middle) {
+	    counter = quartile;
 	}
+	
+	if (low <= middle && quartile <= high) {
+	    if (temp[low] < temp[quartile]) {
+		data[counter] = temp[low];
+		merge(data, temp, low + 1, middle, quartile, high);
+	    }
+
+	    else {  // if (temp[low] >= temp[quartile])
+		data[counter] = temp[quartile];
+		merge(data, temp, low, middle, quartile + 1, high);
+	    }
+	}
+
+	else if (low <= middle) {
+	    data[counter] = temp[low];
+	    merge(data, temp, low + 1, middle, quartile, high);
+	}
+
+	else if (quartile <= high) {
+	    data[counter] = temp[quartile];
+	    merge(data, temp, low, middle, quartile + 1, high);
+	}
+	
+	/* For Testing.
+	print(data);
+	print(temp);
+	System.out.println("Low: " + low + " Middle: " + middle + " High: " + high);
+	*/
+  
+	/* Turning into Selection Sort.
+	   int lowest = 0;
+	   int start = 0;
+
+	   for (int element = 0; index < data.length; element++) {
+	   for (int index = start; index < data.length; index++) {
+	   if 
+	*/
     }
     
     
@@ -79,22 +95,18 @@ public class Merge {
 
     public static void main(String[] args) {
         int[] b = new int[] {17, 61, 93, 67, 47, 93, 4, 12, 20, 4, 12, 44, 68};
-
-	System.out.println("\n" + "----- Swap -----" + "\n");
-        print(b);
-        swap(b, 1, 4);
-        print(b);
+	int[] c = new int[] {8, 23, 7, 2, 568, 48, 2, 468, 347, 2, 37, 1};
+	
         System.out.println("\n" + "----- Merge Sort -----" + "\n");
-        
-	for (int i = 1; i < 10; i++) {            
-            print(b);
-            
-            System.out.println("---");
 
-	    mergesort(b);
-            
-            // b = new int[] {5, 5, 8, 5, 5, 6, 6, 8, 6, 5, 6, 6, 6, 6,5, 6, 8, 5, 8, 8, 5};
-        }
+	print(b);
+	mergesort(b);
+	print(b);
+	System.out.println("---");
+
+	print(c);
+	mergesort(c);
+	print(c);
     }	
     
 }
