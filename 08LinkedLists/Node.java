f51772db8c5968c52faafa6c1595cdfd6c05de79
Alexander Radu <34072014+aradu01@users.noreply.github.com>
2018-03-27 21:07:09 -0400

    Completed Node.java!

---
+++ 08LinkedLists/Node.java
@@ -0,0 +1,23 @@
+public class Node {
+
+    private int data;
+    private Node prev;
+    private Node next;
+    
+    public String toString() {
+        return this.data;
+    }
+    
+    public Node getNext() {
+        return this.next;
+    }
+    
+    public Node getPrev() {
+        return this.prev;
+    }
+    
+    public int getValue() {
+        return this.value;
+    }
+    
+}
