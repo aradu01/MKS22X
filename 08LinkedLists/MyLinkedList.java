92e332c1c67467d3ff0036aa8ce87a39d3aadad3
Alexander Radu <34072014+aradu01@users.noreply.github.com>
2018-03-27 21:40:16 -0400

    Completed add at index.

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -16,6 +16,10 @@ public class MyLinkedList {
         return result + "]";
     }
     
+    public int size() {
+        return length;
+    }
+    
     public int get(int index) {
         if (index < 0 || index >= length) {
             throw new IllegalStateException("Your index must be within the Linked List.");
@@ -32,4 +36,46 @@ public class MyLinkedList {
         return current.getValue();
     }
     
-}       
+    public void set(int index, int value) {
+        if (index < 0 || index >= length) {
+            throw new IllegalStateException("Your index must be within the Linked List.");
+        }
+        
+        int place = 0;
+        Node current = first;
+        
+        while (place < index) {
+            current = current.getNext();
+            place++;
+        }
+        
+        current.setValue(value);
+    }
+    
+    public void add(int index, int value) {
+        if (index < 0 || index >= length) {
+            throw new IllegalStateException("Your index must be within the Linked List.");
+        }
+        
+        int place = 0;
+        Node before = first;
+        Node after = first;
+        
+        while (place < index - 1) {
+            before = before.getNext();
+            after = after.getNext();
+            place++;
+        }
+        after = after.getNext();
+        
+        Node addend = new Node();
+        addend.setValue(value);
+        
+        before.setNext(addend);
+        addend.setPrev(before);
+        
+        addend.setNext(after);
+        after.setPrev(addend);
+    }
+    
+}
