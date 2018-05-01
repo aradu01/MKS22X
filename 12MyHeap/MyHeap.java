aa38927f95bdf647502d12ba914cc3f56fa92095
Alexander Radu <aradu@stuy.edu>
2018-04-30 21:30:05 -0400

    Perfected add method.

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -3,55 +3,104 @@ public class MyHeap<Type extends Comparable<Type>> {
     private Type[] data;
     private int size;
     private boolean minORmax;
+    
+    private static final int CAPACITY = 10;
 
     @SuppressWarnings("unchecked")
     public MyHeap() {
-	data = (Type[]) new Comparable[10];
+        data = (Type[]) new Comparable[CAPACITY];
+        minORmax = true;
     }
     
     @SuppressWarnings("unchecked")        // true entails a max heap.
     public MyHeap(boolean determinant) {  // false entails a min heap.
-	minORmax = determinant;
-	data = (Type[]) new Comparable[10];
+        minORmax = determinant;
+        data = (Type[]) new Comparable[CAPACITY];
+    }
+    
+    public String toString() {
+        String result = "[";
+        
+        if (size == 0) {
+            return "[]";
+        }
+        
+        for (int index = 0; index < size; index++) {
+            result += data[index] + ", ";
+        }
+        
+        return result.substring(0, result.length() - 2) + "]";
     }
 
     public void add(Type addition) {
-	int position;
+        int position = size;
 
-	if (size == data.length) {
-	    expand();
-	}
-	
-	if (minORmax) {
-	    
-	    while (
-	}
+        if (size == data.length) {
+            expand();
+        }
+        
+        data[size] = addition;
+        size++;
+
+        if (minORmax) {
+            while ((position - 1) / 2 >= 0 && data[position].compareTo(data[(position - 1) / 2]) > 0) {
+                swap((position - 1) / 2, position);
+                position = (position - 1) / 2;
+            }
+        }
+        
+        else {
+            while ((position - 1) / 2 >= 0 && data[position].compareTo(data[(position - 1) / 2]) < 0) {
+                swap((position - 1) / 2, position);
+                position = (position - 1) / 2;
+            }
+        }
     }
 
     public Type remove() {
+        return null;
     }
 
     public Type peek() {
-	return data[0];
+        return data[0];
     }
 
     public int size() {
-	return size;
+        return size;
     }
 
     @SuppressWarnings("unchecked")
     private void expand() {
-	Type[] temporary = data;
-	
-	data = (Type[]) new Comparable[data.length * 2];
+        Type[] temporary = data;
 
-	for (int index = 0; index < temporary.length; index++) {
-	    data[index] = temporary[index];
-	}
+        data = (Type[]) new Comparable[data.length * 2];
+
+        for (int index = 0; index < temporary.length; index++) {
+            data[index] = temporary[index];
+        }
+    }
+    
+    private void swap(int first, int second) {
+        Type storage = data[first];
+        
+        data[first] = data[second];
+        data[second] = storage;
     }
     
     public static void main(String[] args) {
-	
+        System.out.println("----- toString -----");
+        
+        MyHeap<Integer> a = new MyHeap<>(true);
+        System.out.println(a);
+        
+        System.out.println();
+        System.out.println("----- add -----");
+        
+        for (int number = 0; number < 15; number++) {
+            a.add(new Integer(number));
+        }
+        
+        System.out.println(a);
     }
     
 }
