5f7689da5e8c6c77070977f711b52328b007755f
Alexander Radu <34072014+aradu01@users.noreply.github.com>
2018-05-08 20:33:20 -0400

    Added neighbors() method.

--- 14MazeSolver/Location.java
+++ 14MazeSolver/Location.java
@@ -23,4 +23,15 @@ public class Location {
 	return previous;
     }
     
+    public Location[] neighbors() {
+	Location[] result = new Location[4];
+	
+	result[0] = new Location(x - 1, y, this);
+	result[1] = new Location(x, y + 1, this);
+	result[2] = new Location(x + 1, y, this);
+	result[3] = new Location(x, y - 1, this);
+	
+	return result;
+    }
+	
 }
