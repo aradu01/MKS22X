62e7fcfcaf693241e8e02c335d71bc55d5897532
Alexander Radu <aradu@stuy.edu>
2018-06-14 01:15:57 -0400

    More Test Cases Passed.

--- 14MazeSolver/Driver.java
+++ 14MazeSolver/Driver.java
@@ -1,45 +1,43 @@
 public class Driver {
+    
+    private static final int TIME = 10;
 
     public static void main(String[] args) {
         MazeSolver a = new MazeSolver("data1.dat");
-        a.movie(100);
+        a.movie(TIME);
         a.setAStar(true);
-        System.out.println(a.aStarStatus());
-        System.out.println(a.aStarStatus());
-        System.out.println(a.aStarStatus());
-        System.out.println(a.aStarStatus());
         System.out.println(a.aStarStatus());
         System.out.println(a.solve(0));
         
         MazeSolver b = new MazeSolver("data2.dat");
-        b.movie(100);
+        b.movie(TIME);
         b.setAStar(true);
-        System.out.println(b.aStarStatus());
-        System.out.println(b.aStarStatus());
-        System.out.println(b.aStarStatus());
-        System.out.println(b.aStarStatus());
         System.out.println(b.aStarStatus());
         System.out.println(b.solve(1));
         
         MazeSolver c = new MazeSolver("data3.dat");
-        c.movie(100);
+        c.movie(TIME);
         c.setAStar(true);
-        System.out.println(c.aStarStatus());
-        System.out.println(c.aStarStatus());
-        System.out.println(c.aStarStatus());
-        System.out.println(c.aStarStatus());
         System.out.println(c.aStarStatus());
         System.out.println(c.solve(2));
         
         MazeSolver d = new MazeSolver("data4.dat");
-        d.movie(100);
+        d.movie(TIME);
         d.setAStar(true);
         System.out.println(d.aStarStatus());
-        System.out.println(d.aStarStatus());
-        System.out.println(d.aStarStatus());
-        System.out.println(d.aStarStatus());
-        System.out.println(d.aStarStatus());
         System.out.println(d.solve(3));
+        
+        MazeSolver e = new MazeSolver("data5.dat");
+        e.movie(1);
+        e.setAStar(true);
+        System.out.println(e.aStarStatus());
+        System.out.println(e.solve(0));
+        
+        MazeSolver f = new MazeSolver("data6.dat");
+        f.movie(50);
+        f.setAStar(true);
+        System.out.println(f.aStarStatus());
+        System.out.println(f.solve(1));
     }
     
 }
